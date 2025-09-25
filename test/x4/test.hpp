ec991a8dc1b54d2fc739e3e4505dd511d967c929
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-09-25 14:02:46 +0900

    Migrate to Catch2

--- test/x4/test.hpp
+++ test/x4/test.hpp
@@ -15,7 +15,14 @@
 #include <boost/spirit/x4/core/move_to.hpp>
 #include <boost/spirit/x4/parse.hpp>
 
-#include <boost/core/lightweight_test.hpp>
+#if BOOST_SPIRIT_CI_IS_B2
+# define CATCH_CONFIG_FAST_COMPILE
+# define CATCH_CONFIG_NO_USE_BUILTIN_CONSTANT_P
+# define DO_NOT_USE_WMAIN
+# include "catch_amalgamated.hpp"
+#else
+# include <catch2/catch_test_macros.hpp>
+#endif
 
 #include <iterator>
 #include <string_view>
